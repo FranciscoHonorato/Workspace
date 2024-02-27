cd7157280de1f9f3ac982a57399955cb8a2aa843
FranciscoHonorato <deassismg@gmail.com>
2024-02-26 21:44:20 -0300

    primeiro commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
+let burguer = document.querySelector('.burguer')
 let itemMenu = document.querySelector('.itens-menu')
 let itemList = document.querySelectorAll('.item')
 
-function clickMenu(){
-    if(itemMenu.style.display == 'none'){
-       itemMenu.style.display = "flex"
-    }else{
-        itemMenu.style.display = "none"
-    }
+function clickMenu() {
+    
+        if (itemMenu.style.display == 'none') {
+            itemMenu.style.display = "flex"
+        } else{
+            itemMenu.style.display = "none"
+        }
 }
 
-for(let item of itemList){
-    item.addEventListener('click',clickMenu)
+if(burguer.style.display === 'flex'){
+    for (let item of itemList) {
+        item.addEventListener('click', clickMenu)
     
+    }
 }
